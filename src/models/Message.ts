e5a4b30092e5c563f70205ca75a7d82d2b969527
snowypy <git@snowyjs.lol>
2024-12-17 21:39:16 +0000

    Add Message model with TypeORM entity definition and relationship to Ticket

---
+++ src/models/Message.ts
@@ -0,0 +1,35 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { Ticket } from "./Ticket";
+
+@Entity()
+export class Message {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    author!: string;
+
+    @Column()
+    username!: string;
+
+    @Column()
+    message!: string;
+
+    @Column()
+    isStaff!: boolean;
+
+    @Column()
+    isAdmin!: boolean;
+
+    @Column()
+    date!: Date;
+
+    @Column()
+    authorAvatar!: string;
+
+    @Column()
+    createdAt!: number;
+
+    @ManyToOne(() => Ticket, (ticket) => ticket.messages, { nullable: true })
+    ticket!: Ticket | null;
+}
