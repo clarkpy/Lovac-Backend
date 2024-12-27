77f0f2d0ea2e64e6ed91e90bdf019dbd46b864d5
snowypy <git@snowyjs.lol>
2024-12-27 01:14:41 +0000

    Add custom color code for 'LOVAC' in logger for improved message clarity

--- src/logger.ts
+++ src/logger.ts
@@ -1,5 +1,6 @@
 const colors = {
     reset: "\x1b[0m",
+    lovac: "\x1b[32m",
     error: {
         prefix: "\x1b[41m\x1b[30m",
         message: "\x1b[31m"
@@ -20,7 +21,7 @@ const colors = {
 
 const log = (message: string, type: 'error' | 'log' | 'success' | 'warning') => {
     const color = colors[type];
-    console.log(`${color.prefix} ${type.toUpperCase()} ${colors.reset} ${color.message}${message}${colors.reset}`);
+    console.log(`${colors.lovac}[LOVAC]${colors.reset} ${color.prefix} ${type.toUpperCase()} ${colors.reset} ${color.message}${message}${colors.reset}`);
 };
 
 export default log;
