dbad518d3855acf746c77af8bcf2733d52fef6b3
snowypy <git@snowyjs.lol>
2024-12-30 18:43:58 +0000

    Add Counter model with ObjectId support for MongoDB integration

---
+++ src/models/Counter.ts
@@ -0,0 +1,13 @@
+import { Entity, ObjectIdColumn, ObjectId, Column } from "typeorm";
+
+@Entity()
+export class Counter {
+    @ObjectIdColumn()
+    id!: ObjectId;
+
+    @Column()
+    name!: string;
+
+    @Column()
+    value!: number;
+}
