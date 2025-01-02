814f6a92f817ddd8b26acbaa2dab10097b3313f7
snowypy <git@snowyjs.lol>
2025-01-02 03:51:55 +0000

    Refactor Counter model to use ObjectId for the id field

--- src/models/Counter.ts
+++ src/models/Counter.ts
@@ -1,9 +1,9 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, ObjectIdColumn, ObjectId, Column } from 'typeorm';
 
 @Entity()
 export class Counter {
-    @PrimaryGeneratedColumn()
-    id!: number;
+    @ObjectIdColumn()
+    id!: ObjectId;
 
     @Column()
     name!: string;
